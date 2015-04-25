1b5322f1cd1a1a43bdc3849a5d332a061d31f8ab
MARCELVIS <lecram_07@hotmail.com>
2015-04-25 19:46:36 -0400

    added the logic to CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,18 @@
 ## special matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   
+   i <- NULL
+   set <- function(y) {
+     x <<-
+	 i <<- NULL
+	 }
+	 
+	 get function() x
+	 setinv <- function(solve) i <<- solve
+	 getinv <- function() i
+	 list(set = set, get = get, setinv = setinv, getinv = getinv )
 }
-
 
 ## Write a short comment describing this function
 
